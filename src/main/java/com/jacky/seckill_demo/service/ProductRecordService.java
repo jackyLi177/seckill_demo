e93e2ee538513de90a3dd41b743042c551ac33ac
liyj <1192964153@qq.com>
2020-12-28 15:21:31 +0800

    redis seckill

--- src/main/java/com/jacky/seckill_demo/service/ProductRecordService.java
+++ src/main/java/com/jacky/seckill_demo/service/ProductRecordService.java
@@ -1,25 +1,17 @@
 package com.jacky.seckill_demo.service;
 
-import com.jacky.seckill_demo.controller.SecKillController;
 import com.jacky.seckill_demo.mapper.ProductRecordMapper;
 import com.jacky.seckill_demo.mapper.ProductStorageMapper;
 import com.jacky.seckill_demo.model.ProductRecord;
 import com.jacky.seckill_demo.model.ProductStorage;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.transaction.Transaction;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description
@@ -35,48 +27,48 @@ public class ProductRecordService {
     @Autowired
     private ProductStorageMapper storageMapper;
     @Autowired
-    private DataSourceTransactionManager dataSourceTransactionManager;
-    @Autowired
-    private TransactionDefinition transactionDefinition;
+    private StringRedisTemplate redis;
 
     Random random = new Random();
 
     private final static Object lock = new Object();
 
-    //    @Transactional
-    public synchronized String buySth(String userId, String productId) {
+    @Transactional
+    public String buySth(String userId, String productId) {
         int num = random.nextInt(10) + 1;
-        // 查库存
-        ProductStorage storage = storageMapper.getByProductId(productId);
-        Long oldAmount = storage.getAmount();
-        if (num > oldAmount) {
-            log.info(userId + " request " + num + " fail");
-            return userId + " request " + num + " fail";
-        }
-        //获取事务状态对象，开启事务
-        TransactionStatus transaction = dataSourceTransactionManager.getTransaction(transactionDefinition);
-        try {
-            storage.setAmount(oldAmount - num);
-            log.info(userId + " request " + num + " " + productId + " 当前 " + oldAmount);
-            // 减库存
-            int res = storageMapper.updateByPrimaryKeySelective(storage);
-            if (res > 0) {
-                ProductRecord productRecord = new ProductRecord();
-                productRecord.setProductId(productId);
-                productRecord.setUserId(userId);
-                productRecord.setNum(num);
-                recordMapper.insert(productRecord);
-                //事务提交
-                dataSourceTransactionManager.commit(transaction);
-                return userId + "request " + num + " success" + " 当前 " + oldAmount;
-            } else {
-                //事务回滚
-                dataSourceTransactionManager.rollback(transaction);
-                return userId + "request " + num + " fail";
+
+        //获取锁
+        Boolean lock = redis.opsForValue().setIfAbsent("skill::" + productId, userId, 3, TimeUnit.SECONDS);
+        if (lock) {
+            try {
+                // 查库存
+                ProductStorage storage = storageMapper.getByProductId(productId);
+                Long oldAmount = storage.getAmount();
+                if (num > oldAmount) {
+                    return userId + " request " + num + " fail";
+                }
+                storage.setAmount(oldAmount - num);
+                log.info(userId + " request " + num + " " + productId + " 当前 " + oldAmount + " success");
+                // 减库存
+                int res = storageMapper.updateByPrimaryKeySelective(storage);
+                if (res > 0) {
+                    ProductRecord productRecord = new ProductRecord();
+                    productRecord.setProductId(productId);
+                    productRecord.setUserId(userId);
+                    productRecord.setNum(num);
+                    recordMapper.insert(productRecord);
+                    return userId + "request " + num + " success" + " 当前 " + oldAmount;
+                } else {
+                    return userId + "request " + num + " fail";
+                }
+            } finally {
+                String currUser = redis.opsForValue().get("skill::" + productId);
+                if (userId.equals(currUser)) {
+                    redis.delete("skill" + productId);
+                }
             }
-        } catch (Exception e) {
-            dataSourceTransactionManager.rollback(transaction);
-        } finally {
+        } else {
+//            log.info(userId + "request lock fail");
             return userId + "request " + num + " fail";
         }
     }
